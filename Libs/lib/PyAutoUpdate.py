72b52fe387ac18673daca9ff6da8279edf962604
liang-work <tianhesl@outlook.com>
2024-12-23 21:31:19 +0800

    Add files via upload

--- Libs/lib/PyAutoUpdate.py
+++ Libs/lib/PyAutoUpdate.py
@@ -3,7 +3,16 @@
 #=======================================#
 import Libs.requests as requests
 
-def check(ufURL,downURL,ver_info):
-    ver_file = requests.get(ufURL)
-    if ver_file.json()["VersionID"] != ver_file["VersionID"] and ver_file.json()["identifier"] != ver_file["identifier"]:
-        pass
+def check(ufURL,ver_info):
+    requests.packages.urllib3.disable_warnings()
+    requests.adapters.DEFAULT_RETRIES = 5
+    s = requests.session()
+    s.keep_alive = False
+    ver_file = requests.get(ufURL,verify=False)
+    if ver_file.json()["versionID"] > ver_info["versionID"] and ver_file.json()["identifier"] != ver_info["identifier"]:
+        return True,ver_file.json()["versionID"]
+    else:
+        return False,ver_info["versionID"]
+
+def upgrade(downURL):
+    return True
